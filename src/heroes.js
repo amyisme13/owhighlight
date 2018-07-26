82383a3d8f024355cf8e617e9dd81f67cc00ab6e
Azmi Makarima <amy.azmim@gmail.com>
2018-07-27 02:05:05 +0800

    Add heroes list placeholder

---
+++ src/heroes.js
@@ -0,0 +1,142 @@
+export default [
+  {
+    name: 'Ana',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Bastion',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Brigitte',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'D.Va',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Doomfist',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Genji',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Hanzo',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Junkrat',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Lúcio',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'McCree',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Mei',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Mercy',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Moira',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Orisa',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Pharah',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Reaper',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Reinhardt',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Roadhog',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Soldier: 76',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Sombra',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Symmetra',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Torbjörn',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Tracer',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Widowmaker',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Winston',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Wrecking Ball',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Zarya',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+  {
+    name: 'Zenyatta',
+    color: 'red',
+    url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+  },
+];
