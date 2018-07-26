530495f1b1c5e92727d3877ce1065949ea64158d
Azmi Makarima <amy.azmim@gmail.com>
2018-07-27 02:08:47 +0800

    update heroes placeholder

--- src/heroes.js
+++ src/heroes.js
@@ -2,141 +2,225 @@ export default [
   {
     name: 'Ana',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Bastion',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Brigitte',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'D.Va',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Doomfist',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Genji',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Hanzo',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Junkrat',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Lúcio',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'McCree',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Mei',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Mercy',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Moira',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Orisa',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Pharah',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Reaper',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Reinhardt',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Roadhog',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Soldier: 76',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Sombra',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Symmetra',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Torbjörn',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Tracer',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Widowmaker',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Winston',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Wrecking Ball',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Zarya',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
   {
     name: 'Zenyatta',
     color: 'red',
+    image:
+      'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
     url: 'https://semantic-ui-vue.github.io/static/images/wireframes/image.png',
+    active: false,
   },
 ];
