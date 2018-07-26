364f323f9074753087c964e3e5e25596f35b9397
Azmi Makarima <amy.azmim@gmail.com>
2018-07-27 02:15:01 +0800

    heroes view

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template lang="pug">
-  #app
-    h1 {{ msg }}
+  #app(is="sui-container")
+    sui-header(
+      size="huge",
+      textAlign="center") AMYisMe Highlights
+
+    sui-card-group(
+      :items-per-row="7")
+      sui-card.red(
+        v-for="hero in heroes",
+        key="hero.name")
+        sui-dimmer-dimmable(
+          @mouseenter.native="hero.active = true"
+          @mouseleave.native="hero.active = false")
+          sui-image(:src="hero.image")
+          sui-dimmer(
+            blurring,
+            :active="hero.active")
+            a(
+              is="sui-button",
+              inverted,
+              :href="hero.url") {{ hero.name }}
 </template>
 
 <script>
+import heroes from './heroes';
+
 export default {
   name: 'App',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App!',
+      heroes,
     };
   },
 };
 </script>
-
-<style>
-#app {
-  color: #56b983;
-}
-</style>
